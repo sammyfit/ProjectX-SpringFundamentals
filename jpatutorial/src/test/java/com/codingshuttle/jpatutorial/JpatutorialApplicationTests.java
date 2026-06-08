a710585bf4560e3c58456e9b1617d6fea07c5be4
Samrat Ganguly <51021560+sammyfit@users.noreply.github.com>
2026-06-09 03:11:24 +0530

    Module 03 - Lecture 02 Understanding Hibernate

---
+++ jpatutorial/src/test/java/com/codingshuttle/jpatutorial/JpatutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codingshuttle.jpatutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpatutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
