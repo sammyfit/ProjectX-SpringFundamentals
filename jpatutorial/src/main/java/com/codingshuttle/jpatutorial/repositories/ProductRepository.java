ceba5eb571dc50eb50b4fd19559ce857b0ab5a93
Samrat Ganguly <51021560+sammyfit@users.noreply.github.com>
2026-06-10 04:15:01 +0530

    Module 03 Lecture 03 - Completed
    spring-data-jpa-interfaces-and-dynamic-query-methods

---
+++ jpatutorial/src/main/java/com/codingshuttle/jpatutorial/repositories/ProductRepository.java
@@ -0,0 +1,19 @@
+package com.codingshuttle.jpatutorial.repositories;
+
+import com.codingshuttle.jpatutorial.entities.ProductEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface ProductRepository extends JpaRepository<ProductEntity, Long> {
+
+    List<ProductEntity> findByTitle(String title);
+
+    @Query("select e from ProductEntity e where e.title=?1 and e.price=?2")
+    Optional<ProductEntity> findByTitleAndPrice(String title, BigDecimal price);
+}
--- jpatutorial/src/test/java/com/codingshuttle/jpatutorial/JpatutorialApplicationTests.java
+++ jpatutorial/src/test/java/com/codingshuttle/jpatutorial/JpatutorialApplicationTests.java
@@ -1,13 +1,41 @@
 package com.codingshuttle.jpatutorial;
 
+import com.codingshuttle.jpatutorial.entities.ProductEntity;
+import com.codingshuttle.jpatutorial.repositories.ProductRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Optional;
 
 @SpringBootTest
 class JpatutorialApplicationTests {
 
+	@Autowired
+	ProductRepository productRepository;
+
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void testRepository() {
+		ProductEntity productEntity = ProductEntity.builder().sku("nestle234").title("nestle chocolate").price(BigDecimal.valueOf(100.50)).quantity(5).build();
+		System.out.println(productRepository.save(productEntity));
+	}
+
+	@Test
+	void getRepository() {
+		List<ProductEntity> entities  = productRepository.findByTitle("nestle chocolate");
+		System.out.println(entities);
+	}
+
+	@Test
+	void getSingleFromRepository() {
+		Optional<ProductEntity> entity  = productRepository.findByTitleAndPrice("nestle chocolate", BigDecimal.valueOf(100.50));
+		System.out.println(entity);
 	}
 
 }
